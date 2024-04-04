06149a64cccab887891075a6aed9e142c337aacf
Aeewow <egor.nazarenko31@mail.ru>
2024-04-04 23:33:16 +0800

    Промежуточные работы

---
+++ app/src/main/java/com/example/newsaapp/db/ArticleDAO.kt
@@ -0,0 +1,23 @@
+package com.example.newsaapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.newsaapp.models.Article
+import retrofit2.http.DELETE
+
+
+@Dao
+interface ArticleDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @DELETE
+    suspend fun deleteArticle(article: Article)
+}
