28a622260dae579f57acae521b6d28eb94fb0c6a
Aeewow <egor.nazarenko31@mail.ru>
2024-04-07 11:35:34 +0800

    Работа с ViewModel

--- app/src/main/java/com/example/newsaapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsaapp/ui/NewsActivity.kt
@@ -2,11 +2,30 @@ package com.example.newsaapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsaapp.R
+import com.example.newsaapp.databinding.ActivityNewsBinding
+import com.example.newsaapp.db.ArticleDatabase
+import com.example.newsaapp.repository.NewsRepository
 
 class NewsActivity : AppCompatActivity() {
+
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var binding: ActivityNewsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val ViewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
+        newsViewModel = ViewModelProvider(this, ViewModelProviderFactory).get(NewsViewModel::class.java)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/example/newsaapp/ui/NewsViewModel.kt
@@ -0,0 +1,134 @@
+package com.example.newsaapp.ui
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.newsaapp.models.Article
+import com.example.newsaapp.models.NewsResponse
+import com.example.newsaapp.repository.NewsRepository
+import com.example.newsaapp.util.Resource
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.Response
+
+class NewsViewModel(app: Application, val newsRepository: NewsRepository): AndroidViewModel(app) {
+
+    val headlines: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var headlinePage = 1
+    var headlinesResponse: NewsResponse? = null
+
+    val searchNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var searchNewsPage = 1
+    var searchNEwsResponse: NewsResponse? = null
+    var newSearchQuery: String? = null
+    var oldSearchQuery: String? = null
+
+    init {
+        getHeadlines("ru")
+    }
+
+    fun getHeadlines(countryCode: String) = viewModelScope.launch {
+        headlinesInternet(countryCode)
+    }
+
+    fun searchNews(searchQuery: String) = viewModelScope.launch {
+        headlinesInternet(searchQuery)
+    }
+
+    private fun handleHeadlinesResponse(response: Response<NewsResponse>): Resource<NewsResponse>{
+        if (response.isSuccessful){
+            response.body()?.let {resultResponse ->
+                headlinePage++
+                if (headlinesResponse == null){
+                    headlinesResponse = resultResponse
+                }else{
+                    val oldArticles = headlinesResponse?.articles
+                    val newArticles = resultResponse.articles
+                    oldArticles?.addAll(newArticles)
+                }
+                return Resource.Success(headlinesResponse ?:resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    private fun handleSearchNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse>{
+        if(response.isSuccessful){
+            response.body()?.let {resultResponse ->
+                if(searchNEwsResponse == null || newSearchQuery != oldSearchQuery){
+                    searchNewsPage = 1
+                    oldSearchQuery = newSearchQuery
+                    searchNEwsResponse = resultResponse
+                }else{
+                    searchNewsPage++
+                    val oldArticles = searchNEwsResponse?.articles
+                    val newArticles = resultResponse.articles
+                    oldArticles?.addAll(newArticles)
+                }
+                return Resource.Success(searchNEwsResponse ?: resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    fun addToFavourites(article: Article) = viewModelScope.launch {
+        newsRepository.upsert(article)
+    }
+
+    fun getFavouriteNews() = newsRepository.getFavouriteNews()
+
+    fun deleteArticle(article: Article) = viewModelScope.launch {
+        newsRepository.deleteArticle(article)
+    }
+
+    fun internetConnection(context: Context): Boolean{
+        (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).apply {
+            return getNetworkCapabilities(activeNetwork)?.run{
+                when{
+                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                    else -> false
+                }
+            } ?: false
+        }
+    }
+
+    private suspend fun headlinesInternet(countryCode: String){
+        headlines.postValue(Resource.Loading())
+        try {
+            if(internetConnection(this.getApplication())){
+                val response = newsRepository.getHeadlines(countryCode, headlinePage)
+                headlines.postValue(handleHeadlinesResponse(response))
+            }else{
+                headlines.postValue(Resource.Error("Нет подсоединения к интернету"))
+            }
+        }catch (t: Throwable){
+            when(t){
+                is IOException -> headlines.postValue(Resource.Error("Не удаётся подключиться"))
+                else -> headlines.postValue(Resource.Error("Нет сигнала"))
+            }
+        }
+    }
+    private suspend fun searchNewsInternet(searchQuery: String){
+        newSearchQuery = searchQuery
+        searchNews.postValue(Resource.Loading())
+        try {
+            if (internetConnection(this.getApplication())){
+                val response = newsRepository.searchNews(searchQuery, searchNewsPage)
+                searchNews.postValue(handleSearchNewsResponse(response))
+            }else{
+                searchNews.postValue(Resource.Error("Нет подключения к интернету"))
+            }
+        }catch (t: Throwable){
+            when(t){
+                is IOException -> searchNews.postValue(Resource.Error("Не удаётся подключиться"))
+                else -> searchNews.postValue(Resource.Error("Нет сигнала"))
+            }
+        }
+    }
+}
