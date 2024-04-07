039c660d7aca501052e1296066d00f70d29a85ce
Aeewow <egor.nazarenko31@mail.ru>
2024-04-07 16:42:38 +0800

    Сижу и думаю, как решить проблему со скобкой

--- app/src/main/java/com/example/newsaapp/util/Constants.kt
+++ app/src/main/java/com/example/newsaapp/util/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object{
         const val API_KEY = "c5a8dfe8c25a43d6ae989e629e83dc48"
         const val BASE_URL = "https://newsapi.org/"
-        const val SEARCH_NEWS_TIME_DELAY = "500L"
-        const val QUERY_PAGE_SIZE = "20"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
